faa8d2a8258202404c1659ffcb34e5ab62ea671d
Victor Allen <vamuigua@gmail.com>
2017-06-28 17:55:21 +0300

    Modified content layout

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 require('sinatra')
-  require('sinatra/reloader')
+require('sinatra/reloader')
+require('./lib/title_case')
+also_reload('lib/**/*.rb')
 
 #route the get request from the browser and respond with our index view.
-  get('/') do
+get('/') do
     erb(:index)
+  end
+#route for title 
+get('/title') do
+    @title = params.fetch('title').title_case()
+    erb(:title)
   end
